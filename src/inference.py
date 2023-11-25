eae01935728c981a6afaed9289c1ccfa52d364cd
Lê Hoàn Chân <Le.Hoan.Chan@gmail.com>
2023-11-25 14:34:46 +0700

    Sửa hàm hiển thị bảng được nhận diện

--- src/inference.py
+++ src/inference.py
@@ -577,48 +577,44 @@ def cells_to_html(cells):
 
     return str(ET.tostring(table, encoding="unicode", short_empty_elements=False))
 
+class_map_color = {
+    'full_lined': (1, 0, 0.6),
+    'partial_lined_merged_cells': (0.6, 1, 0.6),
+    'nolines': (0.6, 0.6, 1),
+    'partial_lined': (1, 0.6, 1),
+    'merged_cells': (0.6, 1, 1),
+    'no object':  (0.6, 0.6, 0.6)
+}
+
 def visualize_detected_tables(img, det_tables, out_path):
     plt.imshow(img, interpolation="lanczos")
     plt.gcf().set_size_inches(20, 20)
     ax = plt.gca()
-    
-    for det_table in det_tables:
-        bbox = det_table['bbox']
 
-        if det_table['label'] == 'table':
-            facecolor = (1, 0, 0.45)
-            edgecolor = (1, 0, 0.45)
-            alpha = 0.3
-            linewidth = 2
-            hatch='//////'
-        elif det_table['label'] == 'table rotated':
-            facecolor = (0.95, 0.6, 0.1)
-            edgecolor = (0.95, 0.6, 0.1)
-            alpha = 0.3
-            linewidth = 2
-            hatch='//////'
-        else:
-            continue
- 
-        rect = patches.Rectangle(bbox[:2], bbox[2]-bbox[0], bbox[3]-bbox[1], linewidth=linewidth, 
-                                    edgecolor='none',facecolor=facecolor, alpha=0.1)
-        ax.add_patch(rect)
-        rect = patches.Rectangle(bbox[:2], bbox[2]-bbox[0], bbox[3]-bbox[1], linewidth=linewidth, 
-                                    edgecolor=edgecolor,facecolor='none',linestyle='-', alpha=alpha)
-        ax.add_patch(rect)
-        rect = patches.Rectangle(bbox[:2], bbox[2]-bbox[0], bbox[3]-bbox[1], linewidth=0, 
-                                    edgecolor=edgecolor,facecolor='none',linestyle='-', hatch=hatch, alpha=0.2)
-        ax.add_patch(rect)
+    for det_table in det_tables:
+      label = det_table['label']
+      if label not in class_map_color: continue
+      bbox = det_table['bbox']
+      facecolor = edgecolor = class_map_color[label]
+      alpha = 0.3
+      linewidth = 2
+      hatch='//////'
+
+      rect = patches.Rectangle(bbox[:2], bbox[2]-bbox[0], bbox[3]-bbox[1], linewidth=linewidth, edgecolor='none',facecolor=facecolor, alpha=0.1)
+      ax.add_patch(rect)
+      rect = patches.Rectangle(bbox[:2], bbox[2]-bbox[0], bbox[3]-bbox[1], linewidth=linewidth, edgecolor=edgecolor,facecolor='none',linestyle='-', alpha=alpha)
+      ax.add_patch(rect)
+      rect = patches.Rectangle(bbox[:2], bbox[2]-bbox[0], bbox[3]-bbox[1], linewidth=0, edgecolor=edgecolor,facecolor='none',linestyle='-', hatch=hatch, alpha=0.2)
+      ax.add_patch(rect)
 
     plt.xticks([], [])
     plt.yticks([], [])
 
-    legend_elements = [Patch(facecolor=(1, 0, 0.45), edgecolor=(1, 0, 0.45),
-                                label='Table', hatch='//////', alpha=0.3),
-                        Patch(facecolor=(0.95, 0.6, 0.1), edgecolor=(0.95, 0.6, 0.1),
-                                label='Table (rotated)', hatch='//////', alpha=0.3)]
-    plt.legend(handles=legend_elements, bbox_to_anchor=(0.5, -0.02), loc='upper center', borderaxespad=0,
-                    fontsize=10, ncol=2)  
+    legend_elements = []
+    for label, color in class_map_color.items():
+      legend_elements+=[Patch(facecolor=color, edgecolor=color, label=label, hatch='//////', alpha=0.3)]
+
+    plt.legend(handles=legend_elements, bbox_to_anchor=(0.5, -0.02), loc='upper center', borderaxespad=0, fontsize=10, ncol=2)
     plt.gcf().set_size_inches(10, 10)
     plt.axis('off')
     plt.savefig(out_path, bbox_inches='tight', dpi=150)
